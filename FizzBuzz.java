f4e6c7678b8c455ecab504cfdf5b5d838b4c42c9
seifmat <jj591128@ens.uqam.ca>
2020-01-24 15:12:03 -0500

    correction de la fonction

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,11 +1,17 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-        for(int i=0 ;i < 100 ; i++)
+
+    public static void afficherFizzBuzz(int n)
+    {
+        for(int i=0 ;i < n ; i++)
         {
             if ( i %15 ==0 ) System.out.println( "fizzbuzz");
             else if (i % 5 ==0) System.out.println( "buzz");
             else if (i % 3 ==0 ) System.out.println( "fizz");
             else System.out.println(i);
         }
+    }
+
+    public static void main(String[] args) {
+        afficherFizzBuzz(200);
     }
 }
