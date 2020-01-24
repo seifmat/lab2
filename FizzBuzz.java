75ec684a0c5b46f41ac26f9017762c034f739c44
seifmat <jj591128@ens.uqam.ca>
2020-01-24 14:58:08 -0500

    correction

---
+++ FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int i=0 ;i < 100 ; i++)
+        {
+            if ( i %15 ==0 ) System.out.println( "fizzbuzz");
+            else if (i % 5 ==0) System.out.println( "buzz");
+            else if (i % 3 ==0 ) System.out.println( "fizz");
+            else System.out.println(i);
+        }
+    }
+}
